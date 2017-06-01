d42d8d6ab9ddba76f7035d6f7f236d535d72f5e2
Chris Coutinho <chrisbcoutinho@gmail.com>
2017-06-01 10:15:49 +0200

    Added initial work for n4

---
+++ rust/n4_largest_palindrome_product/src/digits.rs
@@ -0,0 +1,36 @@
+// Originally gathered from: https://stackoverflow.com/a/41536521/5536001
+
+pub struct Digits {
+    n: usize,
+    divisor: usize,
+}
+
+impl Digits {
+    pub fn new(n: usize) -> Self {
+        let mut divisor = 1;
+        while n >= divisor * 10 {
+            divisor *= 10;
+        }
+
+        Digits {
+            n: n,
+            divisor: divisor,
+        }
+    }
+}
+
+impl Iterator for Digits {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.divisor == 0 {
+            None
+        } else {
+            let v = Some(self.n / self.divisor);
+            self.n %= self.divisor;
+            self.divisor /= 10;
+            v
+        }
+
+    }
+}
--- rust/n4_largest_palindrome_product/src/main.rs
+++ rust/n4_largest_palindrome_product/src/main.rs
@@ -1,3 +1,36 @@
+mod digits;
+use digits::Digits;
+
+fn is_palindrome(n: usize) -> bool {
+    let digits: Vec<usize> = Digits::new(n).collect();
+
+    println!("{:?}", digits);
+    println!("len(digits) = {}", digits.len());
+    println!("len(digits)/2 = {}", digits.len()/2);
+
+    let mut exit = true;
+
+    for i in 0..digits.len()/2 {
+        let dig_i = digits[i];
+        let dig_ni = digits[digits.len()-i-1];
+
+        println!("digits({}) = {}", i, &dig_i);
+        println!("digits({}) = {}", digits.len()-i-1, &dig_ni);
+
+        if &dig_ni != &dig_i { exit = false };
+    }
+
+    return exit
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("is_palindrome({}): {}", 421, is_palindrome(421));
+
+    let v = (1..).take_while(|&x| x < 100)
+                .filter(|&x| is_palindrome(x))
+                .collect::<Vec<usize>>();
+
+    println!("v: {:?}", v);
+
 }
